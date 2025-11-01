a1e5bfb09afb60012a48602dbbea15918d9c5d4b
HenriquePoltronieri <henrique.poltronieri2008@gmail.com>
2025-11-01 15:20:39 -0300

    Update modal

--- scipt.js
+++ scipt.js
@@ -1,3 +1,18 @@
 function darkmode() {
     document.body.classList.toggle("darkmode")
 }
+
+// Modal Projetos
+function openModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.showModal(); 
+    }
+}
+
+function closeModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.close(); 
+    }
+}
